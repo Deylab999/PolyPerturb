4f1bc77141b41bae5f6c2a9c77f600fe6022b1cf
anderseng <genevieve.andersen1@gmail.com>
2024-03-13 17:33:08 -0400

    refactor RWR output & enable softmax seed weighting

--- PolyGene/codes/ppi_string_RWR.R
+++ PolyGene/codes/ppi_string_RWR.R
@@ -1,24 +1,21 @@
 #' @param gene_scores  matrix of gene scores with genes along the rows and gene sets along the columns
 #' @param restart_prob The probability of restart for RWR method (default is 0.5).
 #' @param thresh_score An upper threshold for the scores in the gene score matrix.
-#' @param edgelist_cutoff The cut-off on the edge connection strength. The default is 0.10 but the user is recommended to try other
-#'                        options
-#' @param NUM_RUNS The number of RWR iterations with random initialization based on gene scores.
+#' @param softmax When FALSE, all seed genes are weighted equally. When TRUE, seed genes are weighted by softmax normalization
 #' @param NUM_SUBGENES The number of genes to keep in each sub-sample per run for a gene score. If NULL, we take 80% of genes per gene
 #'                     score, thresholded below at 100.
 #' @results:  A list with two entries : 'score' : The prioritization score between 0 and 1 based on the PPI RWR. We recommend thresholding it
 #' to top X% genes. 'score_sd': Standard deviation of the score.
-#' @import dnet, igraph, here, data.table
+#' @import dnet, igraph, here, data.table, tidyverse
 #'
 #'
 
-
-ppi_string_RWR = function(gene_scores,
+ppi_string_RWR = function(gene_scores = fread(str_c(here(), "/data/MAGMA_v108_GENE_0_ZSTAT.txt")),
                    restart_prob = 0.5,
                    thresh_score = 5,
-                   edgelist_cutoff = 0.10,
+                   softmax = TRUE,
                    NUM_RUNS = 5,
-                   NUM_SUBGENES=NULL){
+                   NUM_SUBGENES=100){ 
 
   library(here)
   library(tidyverse)
@@ -47,19 +44,21 @@ ppi_string_RWR = function(gene_scores,
     rename(symbol1 = symbol) %>%
     left_join(alias_df, by=c("protein2" = "string_id"), relationship = "many-to-many") %>%
     rename(symbol2 = symbol) %>%
-    select(symbol1, symbol2, combined_score)
+    select(symbol1, symbol2, combined_score) %>%
+    filter(symbol1 != "NA") %>%
+    filter(symbol2 != "NA")
     
   #create an empty graph and fill it in from the dataframe
   string_graph <- graph_from_data_frame(string_df, directed = FALSE)
 
   #####################  Subset the gene aliases to proteins that occur in the graph  #######################
-  
-  ## TEST
-  gene_scores = fread(str_c(here(), "/data/MAGMA_v108_GENE_0_ZSTAT.txt"))
+
+  #deal with no gene column name in input gene score file
   if("V1" %in% colnames(gene_scores)){
     gene_scores <- gene_scores %>% rename("gene_symbol" = "V1")
   }
   
+  #throw some warnings if something seems off
   if(!'gene_symbol' %in% colnames(gene_scores)){
     warning("A column named `gene_symbol` was not provided in the input gene_scores data.")
   }
@@ -100,53 +99,18 @@ ppi_string_RWR = function(gene_scores,
   seed_mat <- seed_mat %>% select(-gene_symbol) %>% as.matrix(seed_mat)
   rownames(seed_mat) <- tmp_rownames
   
+  #retain the ability to weight all seed genes equally
+  if(softmax==FALSE){
+    seed_mat <- ifelse(seed_mat != 0, 1, 0)
+  }
+  
   #############  Run RWR    ################################################
   PTmatrix <- dRWR(g=string_graph, normalise="laplacian", setSeeds=seed_mat,
-                     restart=0.5, parallel=TRUE)
+                     restart=restart_prob, parallel=TRUE)
     rownames(PTmatrix) = as_ids(V(string_graph))
     colnames(PTmatrix) = colnames(seed_mat)
     
-  #############  Normalize RWR output    ##################################
-
-    Step1_matrix = apply(PTmatrix, 2, function(x) {
-      y = 1-ecdf(x)(x)
-      return(y)
-    })
-
-    Step2_vec = apply(Step1_matrix, 1, function(x) {
-      y = -2*sum(log(x+1e-08))
-      return(y)
-    })
-
-    combined_p = pchisq(Step2_vec, 2*ncol(Step1_matrix), lower.tail = F)
-    unscaled_score = -log(combined_p)
-    PR = (unscaled_score - min(unscaled_score))/(max(unscaled_score) - min(unscaled_score))
-    names(PR) = as_ids(V(string_graph))
-
-    ids = match(rownames(seed_mat), names(PR))
-    ids = ids[!is.na(ids)]
-    PR2 = PR[ids]
-    PR_gene_names = gene_aliases_3[match(names(PR2), gene_aliases_3[,1]), 2]
-
-    names(PR2) = PR_gene_names
-
-    priority_list[[num_iter]] = PR2
-    cat("We have performed RWR for Iteration:", num_iter, "\n")
-  }
-
-  union_genes = c()
-  for(mm in 1:length(priority_list)){
-    union_genes = c(union_genes, names(priority_list[[mm]]))
-  }
-  union_genes = unique(union_genes)
-
-  priority_matt = matrix(0, length(union_genes), NUM_RUNS)
-  for(mm in NUM_RUNS){
-    priority_matt[match(names(priority_list[[mm]]), union_genes), mm] = as.numeric(priority_list[[mm]])
-  }
-  score = rowMeans(priority_matt)
-  names(score) = union_genes
-  score_sd = apply(priority_matt, 1, sd)
-  outlist = list("score" = score, "score_sd" = score_sd)
+  score_sd = apply(PTmatrix, 1, sd)
+  outlist = list("score" =  as.matrix(PTmatrix), "score_sd" = score_sd)
   return(outlist)
 }
