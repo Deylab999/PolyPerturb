aef3b966fdc4755b02f76f9eb4fb209331b6a7cc
anderseng <genevieve.andersen1@gmail.com>
2024-03-15 18:19:46 -0400

    add Mendelian gene benchmarking files.

--- PolyGene/codes/benchmark_PolyNet_scores.R
+++ PolyGene/codes/benchmark_PolyNet_scores.R
@@ -1,5 +1,6 @@
 library(here)
 library(tidyverse)
+library(data.table)
 
 source(str_c(here(), "/PolyGene/codes/create_string_graph_object.R"))
 source(str_c(here(), "/PolyGene/codes/ppi_string_RWR.R"))
@@ -9,17 +10,9 @@ source(str_c(here(), "/PolyGene/codes/run_pagerank.R"))
 graph <- create_string_graph(edge_threshold = 400)
 test_rwr <- run_RWR(graph=graph, softmax = TRUE, restart_prob = 0.5)
 
-
-#make some fake ground truth data
-true_positives <- bind_cols(gene_symbol = sample(test_rwr$gene_symbol, 500, replace = TRUE),
-                            phenotype = sample(colnames(test_rwr)[2:247], 500, replace = TRUE)) %>%
-  mutate(ground_truth = 1)
-
-true_negatives <- bind_cols(gene_symbol = sample(test_rwr$gene_symbol, 500, replace = TRUE),
-                            phenotype = sample(colnames(test_rwr)[2:247], 500, replace = TRUE)) %>%
-  mutate(ground_truth = 0)
-
-ground_truth <- bind_rows(true_positives, true_negatives)
+#read in the ground truth data
+gt <- fread(str_c(here(),"/data/freund_2018_monogenic_to_complex_ground_truth.csv")) %>%
+  select(-mendelian_disease_group)
 
 #' Compute sensitivity, specificity, precision, recall, Fisher's exact test p-value, and odds ratio.
 #'
@@ -42,24 +35,26 @@ ground_truth <- bind_rows(true_positives, true_negatives)
 #' @import pROC
 #'
 compute_sensitivity_specificity <- function(data = test_rwr,
-                                            ground_truth_df = ground_truth,
-                                            thresholds = seq(0, 1, by = 0.1)) {
+                                            ground_truth_df = gt,
+                                            thresholds = c(0.9, 0.95, 0.99, 0.995)) {
   library(tidyverse)
   library(pROC)
   
   # Calculate percentile ranks for each phenotype
   percentile_ranks <- data %>%
-    mutate(across(-gene_symbol, ~na_if(., NA) %>% replace_na(mean(.)) %>% percent_rank())) %>%
+    mutate(across(-gene_symbol, ~na_if(., NA) %>%
+                    replace_na(mean(.)) %>%
+                    percent_rank())) %>%
     pivot_longer(-gene_symbol, names_to = "phenotype", values_to = "percentile_rank")
   
   # Merge with true positives and true negatives
-  merged_data <- percentile_ranks %>%
-    left_join(ground_truth_df, by = c("gene_symbol", "phenotype")) %>%
+  merged_data <- ground_truth_df %>%
+    left_join(percentile_ranks, by = c("gene_symbol", "phenotype")) %>%
     drop_na()
   
   # Helper function to compute sensitivity, specificity, precision, and recall at many thresholds
-  compute_metrics <- function(data = merged_data, threshold = 0.1) {
-    metric_summary <- data %>%
+  compute_metrics <- function(data_merged_with_gt = merged_data, threshold = 0.9) {
+    metric_summary <- data_merged_with_gt %>%
       group_by(phenotype) %>%
       mutate(predicted_label = ifelse(percentile_rank >= threshold, 1, 0)) %>%
       summarise(
@@ -72,15 +67,7 @@ compute_sensitivity_specificity <- function(data = test_rwr,
         recall = sensitivity
       ) %>%
       ungroup() %>%
-      mutate(threshold = threshold,
-             fisher_test = map2_dbl(TP, n_ground_truths - TP,
-                                    ~fisher.test(matrix(c(.x, .y,
-                                                          sum(ground_truth == 1) - .x, sum(ground_truth == 0) - .y),
-                                                        nrow = 2))$p.value),
-             OR = map2_dbl(TP, n_ground_truths - TP,
-                           ~fisher.test(matrix(c(.x, .y,
-                                                 sum(ground_truth == 1) - .x, sum(ground_truth == 0) - .y),
-                                               nrow = 2))$estimate))
+      mutate(threshold = threshold)
     return(metric_summary)
   }
   
@@ -92,3 +79,9 @@ compute_sensitivity_specificity <- function(data = test_rwr,
 }
 
 test_out <- compute_sensitivity_specificity()
+test_summary <- test_out %>%
+  group_by(threshold) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE))
+test_summary 
+  
+
---
+++ PolyGene/codes/create_mendelian_ground_truth_dataframe.R
@@ -0,0 +1,57 @@
+#' Create Mendelian ground truth mapping file
+#'
+#' This function creates a Mendelian ground truth mapping file based on provided input files.
+#' It identifies "True Negatives" as genes that are in the ground truth file but are not present for any other member of the larger group.
+#'
+#' @param mend_gt_map_file Character string specifying the file path for the Mendelian to GWAS phenotype mapping CSV file.
+#' @param mend_gt_genes_file Character string specifying the file path for the CSV file containing monogenic disease genes by
+#' Mendelian disease group from Freund et al (2018).
+#' @param n_neg Integer specifying the number of negative gene-phenotype pairs to sample for each phenotype (default is 500)
+#' @return A data frame containing the Mendelian ground truth mapping to use in sensitivity/specificity benchmarking.
+#'
+mendelian_ground_truth <- function(mend_gt_map_file = str_c(here(), "/data/mendelian_to_gwas_pheno_mapping.csv"),
+                                   mend_gt_genes_file = str_c(here(),"/data/freund_2018_monogenic_disease_genes.csv"),
+                                   n_neg = 500) {
+  
+  require(data.table)
+  require(dplyr)
+  require(here)
+  
+  mend_gt_map <- fread(mend_gt_map_file)
+  mend_gt_genes <- fread(mend_gt_genes_file)
+  
+  mend_gt <- mend_gt_map %>%
+    drop_na() %>%
+    left_join(mend_gt_genes, by = "mendelian_disease_group", relationship = "many-to-many") %>%
+    select(gene_symbol, phenotype, mendelian_disease_group) %>%
+    drop_na()
+  
+  gt_final <- data.frame()
+  for (p in unique(mend_gt$phenotype)) {
+    
+    positive_df <- mend_gt %>%
+      filter(phenotype == p) %>%
+      mutate(ground_truth = 1)
+    
+    group <- positive_df %>%
+      pull(mendelian_disease_group) %>%
+      unique()
+    
+    negative_genes <- mend_gt %>%
+      filter(!gene_symbol %in% positive_df$gene_symbol) %>%
+      pull(gene_symbol) %>%
+      unique()
+    
+    set.seed(43)
+    negative_df <- bind_cols(
+      gene_symbol = sample(negative_genes, n_neg),
+      phenotype = rep(p, n_neg)
+    ) %>%
+      mutate(mendelian_disease_group = group[1],
+             ground_truth = 0)
+    
+    gt_final <- rbind(gt_final, positive_df, negative_df)
+  }
+  
+  return(gt_final)
+}
